d1113a714038663f5cfd5daad87d4a04ffad6a91
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-04-05 13:24:30 -0400

    commit 158998682 on 20200405

---
+++ ma.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash 
+# Copyright 2019-2020 (c) all rights reserved by BuildAPKs; see LICENSE 
+# https://buildapks.github.io
+################################################################################
+set -Eeuo pipefail
+shopt -s nullglob globstar
+. "$RDR"/scripts/bash/shlibs/trap.bash 210 211 212
+cd "$JDR"
+# _AT_ username/repository commit
+_AT_ 473790228/BlurEffectForAndroidDesign 18565cc259b6ddc6099a63146657db14d70e426b
+_AT_ 473790228/CustomFastScrollViewDemo beab2c37205db68b870e9046025d865163bd5dd8
+# ma.bash OEF
