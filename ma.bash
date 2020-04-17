fdaeb0b223351e669c06649b052a2f3fd8926d1c
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-04-17 14:15:40 -0400

    commit 595500795 on 20200417

--- ma.bash
+++ ma.bash
@@ -10,4 +10,6 @@ cd "$JDR"
 _AT_ 473790228/BlurEffectForAndroidDesign 18565cc259b6ddc6099a63146657db14d70e426b
 _AT_ 473790228/CustomFastScrollViewDemo beab2c37205db68b870e9046025d865163bd5dd8
 _AT_ han1202012/WheelViewDemo 456d577edaf1a3ddc5d28148315949778db146a1
+_AT_ nolanlawson/CustomFastScrollViewDemo beab2c37205db68b870e9046025d865163bd5dd8
+_AT_ romannurik/Android-SwipeToDismiss ecf1e51af2b1f400605b8a3786eed6944dbfbcda
 # ma.bash OEF
